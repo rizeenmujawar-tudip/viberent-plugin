7a4ef35cc6a20e2bcbdcf3d3b3e8093b7ef6f205
rizeenmujawar-tudip <85829777+rizeenmujawar-tudip@users.noreply.github.com>
2022-01-17 19:17:37 +0530

    Update viberent.php

--- viberent.php
+++ viberent.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Login To Viberent
  * 
@@ -6,11 +7,11 @@
  * Plugin URI: https://wordpress.org/plugins/login-to-viberent
  * Description: Login To Viberent is a plugin created to fetch product details from viberent system as per the entered login credentials.
  * Version: 1.0.0
- * Author: Rizeen
+ * Author: Viberent
  * Author URI: https://github.com/rizeenmujawar-tudip/viberent-plugin
  * License: GPLv2 or later
  * Text Domain: viberent login
-*/
+ */
 define('VIBERENT__PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 //register the css files
@@ -22,19 +23,6 @@ function viberent_admin_scripts()
   wp_enqueue_style('font-awesome');
 }
 add_action('admin_enqueue_scripts', 'viberent_admin_scripts');
-
-// function theme_scripts()
-// {
-//   wp_enqueue_script('jquery');
-// }
-// add_action('admin_enqueue_scripts', 'theme_scripts');
-
-// function mytheme_enqueue_typekit()
-// {
-//   wp_register_style('viberent_style', plugins_url('assets/js/moment.min.js', __FILE__));
-//   wp_enqueue_style('viberent_style');
-// }
-// add_action('wp_enqueue_scripts', 'mytheme_enqueue_typekit');
 
 include_once('functions-layout-api.php');
 include_once('viberent-hook.php');
