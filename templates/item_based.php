2b4024d9378ee61815248066737ec8c36cd5c17c
rizeenmujawar-tudip <85829777+rizeenmujawar-tudip@users.noreply.github.com>
2022-01-17 19:26:52 +0530

    Update item_based.php

--- templates/item_based.php
+++ templates/item_based.php
@@ -2,29 +2,46 @@
 session_start();
 /* Template name: Viberent item-based layout */
 require_once('item_page.php');
-require_once('head.php');
+get_header();
 ?>
-
-<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-	<link rel="stylesheet" href="<?php echo plugins_url(); ?>/viberent/assets/css/item.css" type="text/css" media="screen" />
-	<nav id="my_main_nav" class="navbar navbar-light bg-light sticky-top justify-content-between py-0 px-2 px-sm-5">
-		<a class="navbar-brand p-0" href="#"><img class='logo-image' src='data:image/jpeg;base64, <?php echo esc_attr($logo); ?>' /></a>
-		<a id="btn_mycart" class="btn_mycart pt-1" href="<?php echo site_url() . "/my-cart/" ?>">
-			<span class="fa-stack fa-2x has-badge cart" data-count="0">
-				<i class="fa fa-shopping-cart fa-stack-1x"></i>
-			</span>
-		</a>
-	</nav>
-
-	<div id="main-container" class="container-fluid px-2 px-sm-5 py-4">
+<link rel="stylesheet" href="<?php echo plugin_dir_url('item.css', __FILE__ ); ?>viberent/assets/css/item.css" type="text/css" media="screen" />
+<script>
+	jQuery('document').ready(function($) {
+		$(".item-category-box").each(function(index, elem) {
+			$('select#period option[value="<?php if (isset($_POST["period"])) {
+												echo esc_js($_POST["period"]);
+											} elseif (isset($_POST["rentalratesName"])) {
+												echo esc_js($_POST["rentalratesName"]);
+											} else {
+												echo esc_js($firstRental_period);
+											} ?>"]').attr("selected", true);
+		});
+		<?php if (isset($_POST["rentalratesName"])) { ?>
+			$('#my-dates').click();
+		<?php } ?>
+		var totalQuantity = $("#totalQuantity").val();
+		if (totalQuantity > 0) {
+			$(".btn_mycart").find("span.has-badge").attr('data-count', totalQuantity);
+		} else {
+			$(".btn_mycart").find("span.has-badge").attr('data-count', '0');
+		}
+	});
+</script>
+<div class="viberent_item_layout">
+	<div class="main-container container px-2 px-sm-5 py-5">
+		<div class="d-flex justify-content-end">
+            <a class="btn_mycart pt-1" href="<?php echo site_url() . "/my-cart/" ?>">
+                <span class="fa-stack fa-2x has-badge cart" data-count="0">
+                    <i class="fa fa-shopping-cart fa-stack-1x"></i>
+                </span>
+            </a>
+        </div>
 		<div class="row">
 			<div class="col-sm-12 col-md-4 col-lg-3">
 				<div class="rental-period">
 					<form method="post" class="pt-3">
-
 						<label for="period">Rental Period:</label>
-						<select name="period" id="period" required>
-
+						<select name="period" id="period" class="period" required>
 							<?php
 							foreach ($respperiod as $retrieved_period) {
 							?>
@@ -42,16 +59,13 @@ require_once('head.php');
 											month = '' + (d.getMonth() + 1),
 											day = '' + d.getDate(),
 											year = d.getFullYear();
-
 										if (month.length < 2)
 											month = '0' + month;
 										if (day.length < 2)
 											day = '0' + day;
-
 										return [year, month, day].join('-');
 									}
 									jQuery('document').ready(function($) {
-										// alert(convert("Fri Dec 24 2021 00:00:00 GMT+0530 (India Standard Time)"));
 										var dtToday = new Date();
 										var month = dtToday.getMonth() + 1;
 										var day = dtToday.getDate();
@@ -90,7 +104,6 @@ require_once('head.php');
 															}
 														}
 													}
-
 													$("#end-date").val(convert(endDate));
 												} else if (period == "Exclude Sat / Sun Daily") {
 													var start_date = new Date(start_date);
@@ -102,14 +115,12 @@ require_once('head.php');
 													} else {
 														endDate = new Date(start_date.setDate(start_date.getDate() + parseInt(exclude) - 1));
 													}
-
 													$("#end-date").val(convert(endDate));
 												} else if (period == "Exclude Sun") {
 													start_date = new Date(start_date.replace(/-/g, "/"));
 													var endDate = "",
 														noOfDaysToAdd = parseInt(exclude),
 														count = 1;
-
 													if (start_date.getDay() == 0) {
 														endDate = new Date(start_date.setDate(start_date.getDate() + parseInt(exclude)));
 													} else {
@@ -121,7 +132,6 @@ require_once('head.php');
 															}
 														}
 													}
-													//alert(endDate);
 													$("#end-date").val(convert(endDate));
 												} else if (period == "Daily") {
 													var someDate = new Date(start_date);
@@ -132,13 +142,11 @@ require_once('head.php');
 													var getFullYear = start_date.substr(0, 4);
 													var getMonth = start_date.substr(5, 2);
 													var getDate = start_date.substr(8, 2);
-
 													var endDate = "";
 													getMonth = parseInt(getMonth)
 													var lastday = function(y, m) {
 														return new Date(y, m + 0, 0).getDate();
 													}
-
 													if (getDate == 31 && getMonth == 01) {
 														var exclude_date = 29;
 													} else if (getDate == 31 && ((getMonth == 03) || (getMonth == 05) || getMonth == 08 || getMonth == 10 || getMonth == 11)) {
@@ -146,7 +154,6 @@ require_once('head.php');
 													} else if (getDate == 30 && getMonth == 09) {
 														var exclude_date = 30;
 													} else {
-														//alert(lastday(getFullYear, getMonth));
 														var exclude_date = lastday(getFullYear, getMonth);
 													}
 													var someDate = new Date(start_date);
@@ -171,7 +178,6 @@ require_once('head.php');
 											var rental_period = '<?php echo $retrieved_period["name"] ?>';
 											var exclude = '<?php echo $retrieved_period["value"] ?>';
 											var period = $("#period").val();
-
 											if (rental_period == period) {
 												if (period == "Exclude Sat / Sun") {
 													start_date = new Date(start_date);
@@ -194,7 +200,6 @@ require_once('head.php');
 															}
 														}
 													}
-
 													$("#end-date").val(convert(endDate));
 												} else if (period == "Exclude Sat / Sun Daily") {
 													var start_date = new Date(start_date);
@@ -206,7 +211,6 @@ require_once('head.php');
 													} else {
 														endDate = new Date(start_date.setDate(start_date.getDate() + parseInt(exclude) - 1));
 													}
-
 													$("#end-date").val(convert(endDate));
 												} else if (period == "Exclude Sun") {
 													start_date = new Date(start_date.replace(/-/g, "/"));
@@ -225,7 +229,6 @@ require_once('head.php');
 															}
 														}
 													}
-													//alert(endDate);
 													$("#end-date").val(convert(endDate));
 												} else if (period == "Daily") {
 													var someDate = new Date(start_date);
@@ -250,7 +253,6 @@ require_once('head.php');
 													} else if (getDate == 30 && getMonth == 09) {
 														var exclude_date = 30;
 													} else {
-														//alert(lastday(getFullYear, getMonth));
 														var exclude_date = lastday(getFullYear, getMonth);
 													}
 													var someDate = new Date(start_date);
@@ -290,24 +292,12 @@ require_once('head.php');
 							}
 							?>
 						</select>
-
-
 						<label for="start-date">Start Date:</label>
-						<input type="date" data-date="" data-date-format="<?php echo esc_attr($date_Format); ?>" value="<?php if (isset($_POST['start-date'])) {
-																															echo esc_attr($_POST['start-date']);
-																														} else {
-																															echo esc_attr($startFrom_date);
-																														} ?>" id="start-date" name="start-date" placeholder="Select Start Date" required>
-
+						<input type="date" data-date="" data-date-format="<?php echo esc_attr($date_Format); ?>" value="<?php if (isset($_POST['start-date'])) { echo esc_attr($_POST['start-date']); } else { echo esc_attr($startFrom_date); } ?>" id="start-date" name="start-date" placeholder="Select Start Date" required>
 						<label for="end-date">End Date:</label>
-						<input type="date" data-date="" data-date-format="<?php echo esc_attr($date_Format); ?>" value="<?php if (isset($_POST['end-date'])) {
-																															echo esc_attr($_POST['end-date']);
-																														} else {
-																															echo esc_attr($startEnd_date);
-																														} ?>" id="end-date" name="end-date" placeholder="Select End Date" required>
+						<input type="date" data-date="" data-date-format="<?php echo esc_attr($date_Format); ?>" value="<?php if (isset($_POST['end-date'])) { echo esc_attr($_POST['end-date']); } else { echo esc_attr($startEnd_date); } ?>" id="end-date" name="end-date" placeholder="Select End Date" required>
 						<button class="p-0" type="submit" name="my-dates" id="my-dates" style="visibility: hidden;">Check Availability</button>
 					</form>
-
 				</div>
 			</div>
 			<script>
@@ -319,11 +309,8 @@ require_once('head.php');
 					)
 				}).trigger("change")
 			</script>
-
 			<div class="col-sm-12 col-md-8 col-lg-9">
-
 				<h5 class="new-booking">New Booking: <span>
-
 						<?php
 						if (isset($_POST["period"])) {
 							$rentalPeriod = sanitize_text_field($_POST["period"]);
@@ -332,10 +319,8 @@ require_once('head.php');
 						}
 						$my_from_date = date("j/M/Y");
 						$my_to_date = date("Y-m-d", strtotime($firstRental_showValue));
-
 						$show_from_date = date($dateFormat);
 						$show_to_date = date($dateFormat, strtotime($firstRental_showValue));
-
 						if (isset($_POST["my-dates"])) {
 							$my_from_date = sanitize_text_field($_POST["start-date"]);
 							$my_to_date = sanitize_text_field($_POST["end-date"]);
@@ -349,34 +334,25 @@ require_once('head.php');
 							$start_from_date = date("Y-m-d");
 							$end_to_date = date("Y-m-d", strtotime($firstRental_showValue));
 						}
-
 						echo esc_html($show_from_date . ' - ' . $show_to_date);
-
 						?>
-
 					</span> </h5>
-
 				<?php
-
 				if (isset($resp2)) {
 					foreach ($resp2 as $retrieved_data) {
 						$curlavail = wp_remote_get('https://viberent-api.azurewebsites.net/api/Item/item-availability?itemGUID=' . $retrieved_data["itemGUID"] . '&companyid=' . $companyID . '&fromDate=' . $my_from_date . '&todate=' . $my_to_date . '&PeriodTypeId=27&locationID=0', $api_args);
-
 						if (is_wp_error($curlavail) || wp_remote_retrieve_response_code($curlavail) != 200) {
 							return false;
 						}
-
 						$responseavail = wp_remote_retrieve_body($curlavail);
 						$respavail = json_decode($responseavail, 1);
 				?>
 						<div class="item-category-box ng-star-inserted px-3 px-xl-5">
 							<form method="post" action="<?php echo site_url();
 														if (isset($query['pageno'])) { ?>/<?php echo $mypagename; ?>?pageno=<?php echo $query['pageno'];
-																														} else { ?>/<?php echo $mypagename; ?>?pageno=1<?php } ?>&action=add&GUID=<?php echo trim($retrieved_data['itemGUID']); ?>&rental_period=<?php echo $rentalPeriod; ?>">
+														} else { ?>/<?php echo $mypagename; ?>?pageno=1<?php } ?>&action=add&GUID=<?php echo trim($retrieved_data['itemGUID']); ?>&rental_period=<?php echo $rentalPeriod; ?>">
 								<div class="item-info">
-
 									<div class="item-details">
-
 										<div class="product-title">
 											<h5 class="product-name"><?php echo esc_html($retrieved_data["itemName"]); ?></h5>
 											<h5 class="product-pricing m-0">
@@ -431,7 +407,6 @@ require_once('head.php');
 													<?php
 													}
 													if ($is_daily !== 1) {
-
 													?><div class="price-not-available"><?php echo "pricing not available"; ?></div>
 														<input type="hidden" name="price" class="rentalratesvalue" value="<?php echo 0; ?>" />
 												<?php
@@ -440,17 +415,14 @@ require_once('head.php');
 												?>
 											</h5>
 										</div>
-
 										<p class="product-available pt-0">
 											<?php
 											echo "Available: <span class='product_available'>" . esc_attr($respavail[0]['available']) . "</span>";
 											?>
 										</p>
-
 										<div class="add-to-cart-component buy-items-btn ng-star-inserted">
 											<input type="number" class="product-quantity" name="quantity" min="1" value="1" size="2" /><input type="submit" name="add_to_cart" value="Add to Cart" class="btnAddAction" />
 										</div>
-
 										<div class="product-quantity-message">
 											<?php
 											if (isset($_SESSION["cart_item"])) {
@@ -467,15 +439,10 @@ require_once('head.php');
 											}
 											?>
 										</div>
-
 										<div class="item-summary">
-
 											<p class="minimize"><?php echo esc_html($retrieved_data["itemDescription"]); ?></p>
-
 										</div>
-
 									</div>
-
 									<div class="product-image">
 										<img src=<?php
 													if (empty($retrieved_data["images"])) {
@@ -491,7 +458,6 @@ require_once('head.php');
 													}
 													?>>
 									</div>
-
 								</div>
 								<input type="hidden" name="image" value="<?php if (empty($retrieved_data["images"])) {
 																				echo $full_path . 'assets/images/no_image.png';;
@@ -517,16 +483,10 @@ require_once('head.php');
 								<input type="hidden" name="sessionID" value="<?php echo trim($retrieved_data['itemGUID'] . $rentalPeriod); ?>" />
 							</form>
 						</div>
-
-
 					<?php
 					}
-
-
 					$are_pages = $retrieved_data["totalRows"] % 10;
 					$not_final_pages = intval($retrieved_data["totalRows"] / 10);
-
-
 					if ($are_pages == 0) {
 						$total_pages = (int)$not_final_pages;
 					} else {
@@ -540,12 +500,10 @@ require_once('head.php');
 						<?php
 						$query = $_GET;
 						$pagLink = "";
-
 						if ($total_pages > 1) {
 							if ($page_nos >= 2) {
 								echo "<li class='prev'><span><a href='" . site_url() . "/" . $mypagename . "?pageno=" . ($page_nos - 1) . "'>Prev</a></span></li>";
 							}
-
 							for ($x = 1; $x <= $page_nos; $x++) {
 								$query['pageno'] =  $x;
 								$query_result = http_build_query($query);
@@ -554,10 +512,9 @@ require_once('head.php');
 										. $x . "'>" . $x . " </a></span></li>";
 								} else {
 									$pagLink .= "<li><span><a href='" . site_url() . "/" . $mypagename . "?pageno=" . $x . "'>   
-                                                        " . $x . " </a></span></li>";
+                                                    " . $x . " </a></span></li>";
 								}
 							}
-
 							if ($page_nos < $total_pages) {
 								$pagLink .= '<li class="disabled"><span>...</span></li>';
 								$pagLink .= "<li><span><a href='" . site_url() . "/" . $mypagename . "?pageno=" . ($page_nos + 1) . "'>Next</a></span></li>";
@@ -566,19 +523,16 @@ require_once('head.php');
 						}
 					}
 						?>
-
 						</ul>
 					</div>
-
 			</div>
 		</div>
-
 	</div>
-	<!-- Loading Spinner Wrapper-->
+</div>
+<!-- Loading Spinner Wrapper-->
+<div class="loading-spinner">
 	<div class="loader text-center">
 		<div class="loader-inner">
-
-			<!-- Animated Spinner -->
 			<div class="lds-roller mb-3">
 				<div></div>
 				<div></div>
@@ -589,11 +543,9 @@ require_once('head.php');
 				<div></div>
 				<div></div>
 			</div>
-
-			<!-- Spinner Description Text [For Demo Purpose]-->
 			<h4 class="text-uppercase font-weight-bold">Loading Data</h4>
 			<p class="font-italic text-muted">This loading window will be removed after <strong class="countdown text-dark font-weight-bold">7 </strong> Seconds</p>
 		</div>
 	</div>
-</body>
-</html>
+</div>
+<?php get_footer(); ?>
