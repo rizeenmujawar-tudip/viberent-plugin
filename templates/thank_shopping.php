b64909a27a0690f0f28656de5c5e05b4c6e683a3
rizeenmujawar-tudip <85829777+rizeenmujawar-tudip@users.noreply.github.com>
2022-01-17 19:22:54 +0530

    Update thank_shopping.php

--- templates/thank_shopping.php
+++ templates/thank_shopping.php
@@ -1,34 +1,18 @@
 <?php
-
-/*
-Template name: Viberent thank-shopping
- */
+/* Template name: Viberent thank-shopping */
 session_start();
+get_header();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Thank You</title>
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
-    <script src="<?php echo plugins_url(); ?>/viberent/assets/js/jquery.js"></script>
-    <link rel="stylesheet" href="<?php echo plugins_url(); ?>/viberent/assets/css/thank.css" type="text/css" media="screen" />
-      <script>
-        jQuery(document).ready(function() {
-            localStorage.clear();
-        });
-    </script>
-</head>
-
-<body>
+<script>
+  jQuery(document).ready(function() {
+    localStorage.clear();
+  });
+</script>
 <?php
 if (isset($_SESSION["cart_item"])) {
   $total_quantity = 0;
   $total_price = 0;
-  $result = $wpdb->get_results("SELECT * from wp_viberent_clients_company_info");
+  $result = $wpdb->get_results("SELECT * from " . $wpdb->prefix . "viberent_clients_company_info");
   $currencysymbol = $result[0]->currencysymbol;
   $dateFormatfromAPi = $result[0]->dateFormat;
   if ($dateFormatfromAPi == "dd/MM/yyyy") {
@@ -39,23 +23,21 @@ if (isset($_SESSION["cart_item"])) {
     $dateFormat = "m-j-Y";
   }
   $all_items = array();
-  $result = $wpdb->get_results("SELECT * from wp_viberent_clients_company_info");
   $companyID = $result[0]->companyID;
-
-  $resapikey = $wpdb->get_results("SELECT * from wp_viberent_apikey");
+  $resapikey = $wpdb->get_results("SELECT * from " . $wpdb->prefix . "viberent_apikey");
   $apikey = $resapikey[0]->apikey;
-  $api_args = array( 'timeout' => 10,
-      'headers'     => array(
-        'ApiKey' => $apikey,
-          'CompanyId' => $companyID
-      )
-  ); 
+  $api_args = array(
+    'timeout' => 10,
+    'headers'     => array(
+    'ApiKey' => $apikey,
+    'CompanyId' => $companyID
+    )
+  );
   foreach ($_SESSION["cart_item"] as $item) {
     $getcode = $item["code"];
     $mystartDate = date($dateFormat, strtotime($item["startDate"]));
     $myendDate = date($dateFormat, strtotime($item["endDate"]));
     $rentalp = $item["rental_period"];
-
     if ($item["productAvailble"] >= $item["quantity"]) {
       $myquanti = $item["quantity"];
     } else {
@@ -75,8 +57,8 @@ if (isset($_SESSION["cart_item"])) {
     $each_item = array("from" => $item["startDate"], "to" => $item["endDate"], "itemGUID" => $item["GUID"], "itemCode" => $item["code"], "price" => $item["price"], "itemHireTypeID" => $myHireID, "quantity" => $myquanti, "location" => $item["locationID"]);
     array_push($all_items, $each_item);
   }
-$resulty = $wpdb->get_results("SELECT * from wp_viberent_post_array WHERE `custoname` IS NOT NULL");
-if(!empty($resulty)){
+  $resulty = $wpdb->get_results("SELECT * from " . $wpdb->prefix . "viberent_post_array WHERE `custoname` IS NOT NULL");
+  if (!empty($resulty)) {
     $data = array('customerName' => $resulty[0]->custoname, 'companyid' => $resulty[0]->companyID, "billingAddresses" => array("isBilling" => true, "addressType" => "BillTo", "billAddrDtls" => $resulty[0]->billing_address, "city" => $resulty[0]->city_bill, "state" => $resulty[0]->state_bill, "postalCode" => $resulty[0]->postalCode_bill, "country" => $resulty[0]->country_bill, "email" => $resulty[0]->email_bill, "phone" => $resulty[0]->phone_bill, "contactName" => $resulty[0]->custoname), "shipingAddresses" => array("isBilling" => true, "addressType" => "ShipTo", "billAddrDtls" => $resulty[0]->shipping_address, "city" => $resulty[0]->city_bill, "state" => $resulty[0]->state_ship, "postalCode" => $resulty[0]->postalCode_ship, "country" => $resulty[0]->country_ship, "email" => $resulty[0]->email_ship, "phone" => $resulty[0]->phone_ship, "contactName" => $resulty[0]->custoname), "items" => $all_items);
 
     $curlUrl = wp_remote_post('https://viberent-api.azurewebsites.net/api/Quote/create', array(
@@ -85,48 +67,40 @@ if(!empty($resulty)){
       'method'      => 'POST',
       'data_format' => 'body',
     ));
-
     if (is_wp_error($curlUrl) || wp_remote_retrieve_response_code($curlUrl) != 200) {
       return false;
     }
     $response = wp_remote_retrieve_body($curlUrl);
-
     $respquote = json_decode($response, 1);
-
     if (isset($respquote['QuoteNumber'])) {
-      $wpdb->insert('wp_quote_number', $respquote);
-  }}
+      $wpdb->insert($wpdb->prefix . 'quote_number', $respquote);
+    }
+  }
 }
-
 ?>
-    <div id="thanku">
-
-        <h1><i>Thank you for shopping with us!</i></h1>
-        <h4 style="color: green;">Your order was successfully placed</h4><?php
-        $result = $wpdb->get_results("SELECT QuoteNumber from wp_quote_number WHERE `id` IS NOT NULL");
-
-        if(isset($result[0]->QuoteNumber)) {?>
-            <p>Please note your Quote Number for future reference</p><?php
-
-            echo "<h3><b>Your Quote Number </b></h3><h3 style=font-weight:normal;>";
-            echo $result[0]->QuoteNumber. "</h3";
-            $wpdb->query("TRUNCATE TABLE `wp_quote_number`");
-            unset($_SESSION["cart_item"]);
-            $wpdb->query("TRUNCATE TABLE `wp_tbl_product`");
-            $wpdb->query("TRUNCATE TABLE `wp_viberent_post_array`");
-
-            }
-
-            $resuli = $wpdb->get_results("SELECT * from wp_viberent_pagename");
-            $slug_name = sanitize_title($resuli[0]->pagename);
-
-        ?>
-
-    </div>
-
+<div class="viberent-thanku-section">
+  <div id="thanku" class="thanku-section">
+    <h1><i>Thank you for shopping with us!</i></h1>
+    <h4>Your order was successfully placed</h4>
+   <div class="quoteNumer">
+    <?php
+      $result = $wpdb->get_results("SELECT QuoteNumber from " . $wpdb->prefix . "quote_number WHERE `id` IS NOT NULL");
+      if (isset($result[0]->QuoteNumber)) { ?>
+        <p>Please note your Quote Number for future reference</p><?php
+        echo "<h3><b>Your Quote Number </b><span style='font-weight:normal;display:block'>";
+        echo $result[0]->QuoteNumber . "</span></h3";
+        $wpdb->query("TRUNCATE TABLE " . $wpdb->prefix  . "quote_number");
+        unset($_SESSION["cart_item"]);
+        $wpdb->query("TRUNCATE TABLE " . $wpdb->prefix  . "tbl_product");
+        $wpdb->query("TRUNCATE TABLE " . $wpdb->prefix  . "viberent_post_array");
+      }
+        $resuli = $wpdb->get_results("SELECT * from " . $wpdb->prefix . "viberent_pagename");
+        $slug_name = sanitize_title($resuli[0]->pagename);
+    ?>
+  </div>
     <div id="continue_btn">
-        <a href="<?php echo site_url() . "/" . $slug_name; ?>" id="btn_shop_now">Continue Shopping</a>
+      <a href="<?php echo site_url() . "/" . $slug_name; ?>" id="btn_shop_now">Continue Shopping</a>
     </div>
-
-</body>
-</html>
+  </div>
+</div>
+<?php get_footer(); ?>
